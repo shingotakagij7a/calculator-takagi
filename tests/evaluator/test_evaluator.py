44774892746b506315a8c5517cb30bdc622f9ee8
shingotakagij7a <shingo.takagi.j7a@jp.denso.com>
2024-02-23 20:19:39 +0900

    refactor: #3 parameterized test expressions

--- tests/evaluator/test_evaluator.py
+++ tests/evaluator/test_evaluator.py
@@ -5,36 +5,13 @@ from calculator_cli.evaluator.evaluator import Evaluator
 
 class TestEvaluator:
     @pytest.mark.parametrize(
+        ["input", "expected"],
         [
-            "input",
-            "expected"
-        ],
-        [
-            pytest.param(
-                "0",
-                0,
-                id="single operand"
-            ),
-            pytest.param(
-                "1 + 2",
-                3,
-                id="addition"
-            ),
-            pytest.param(
-                "1 - 2",
-                -1,
-                id="subtraction"
-            ),
-            pytest.param(
-                "1 - 2 * 3",
-                -5,
-                id="combination of subtraction and multiplication"
-            ),
-            pytest.param(
-                "(1 - 2) * 3",
-                -3,
-                id="combination of subtraction and multiplication with parentheses"
-            ),
+            pytest.param("0", 0, id="single operand"),
+            pytest.param("1 + 2", 3, id="addition"),
+            pytest.param("1 - 2", -1, id="subtraction"),
+            pytest.param("1 - 2 * 3", -5, id="combination of subtraction and multiplication"),
+            pytest.param("(1 - 2) * 3", -3, id="combination of subtraction and multiplication with parentheses"),
         ]
     )
     def test_文字列の算術演算式の入力が適切な場合にその計算結果の数値を返す(self, input, expected):
